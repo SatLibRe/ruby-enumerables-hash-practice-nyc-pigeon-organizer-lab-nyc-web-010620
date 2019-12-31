4bd78cf3e89f3d76dbf9da334b9d9fa3dd755e16
Miles Higbie <miles.higbie@gmail.com>
2019-12-31 16:53:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,2 @@
 require "pry"
 
-def nyc_pigeon_organizer(pigeon_data)
-  new_hash = {}
-    pigeon_data.each do |c_g_l,data|
-      data.each do |stats,all_names|
-        all_names.each do |name|
-          if new_hash[name] == nil 
-            new_hash[name] = {}
-          end 
-          if new_hash[name][c_g_l] == nil 
-              new_hash[name][c_g_l] = []
-          end 
-          new_hash[name][c_g_l].push(stats.to_s)
-        end 
-      end 
-    end
-    new_hash
-end
